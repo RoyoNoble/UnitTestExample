48c9e9bdf1e68639ced48a829ea246d1c51de602
RoyoNoble <cotepich@gmail.com>
2017-03-13 20:47:38 -0700

    Upload

---
+++ UnitTestExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static long Hashing(String s)
+        {
+            long h = 7;
+            var letters = "acdegilmnoprstuw";
+            var index = 0;
+            long inc = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                inc = h * 37;
+                index = letters.IndexOf(s[i]);
+                h = inc + index;
+            }
+
+
+            return h;
+        }
+    }
+}
---
+++ UnitTestExampleTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void HashingTest()
+        {
+            Assert.AreEqual(680131659347, Program.Hashing("leepadg"));
+        }
+    }
+}
